6e8bb7587a0699268add75797ddb730e1e35eb74
huangda7502002 <353656864@qq.com>
2022-08-09 20:13:21 +0800

    init

--- gmall-realtime/src/main/java/com/atguigu/app/dim/DimApp.java
+++ gmall-realtime/src/main/java/com/atguigu/app/dim/DimApp.java
@@ -2,19 +2,18 @@ package com.atguigu.app.dim;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.atguigu.app.func.MyBroadcastFunction;
-import com.atguigu.app.func.MyPhoenixSink;
+import com.atguigu.app.func.DimSinkFunction;
+import com.atguigu.app.func.TableProcessFunction;
 import com.atguigu.bean.TableProcess;
 import com.atguigu.utils.MyKafkaUtil;
 import com.ververica.cdc.connectors.mysql.source.MySqlSource;
 import com.ververica.cdc.connectors.mysql.table.StartupOptions;
-import com.ververica.cdc.debezium.DebeziumSourceFunction;
 import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.api.common.state.MapStateDescriptor;
-import org.apache.flink.runtime.state.filesystem.FsStateBackend;
+import org.apache.flink.api.common.time.Time;
+import org.apache.flink.runtime.state.hashmap.HashMapStateBackend;
 import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.BroadcastConnectedStream;
 import org.apache.flink.streaming.api.datastream.BroadcastStream;
@@ -22,63 +21,86 @@ import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
 import org.apache.flink.util.Collector;
+import org.apache.flink.util.OutputTag;
+
+import java.util.concurrent.TimeUnit;
 
 public class DimApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // 1. 获取执行环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
-        env.enableCheckpointing(5 * 60000L, CheckpointingMode.EXACTLY_ONCE);
-        env.getCheckpointConfig().setCheckpointTimeout(10 * 60000L);
-        env.getCheckpointConfig().setMaxConcurrentCheckpoints(2);
-        env.getCheckpointConfig().enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
-        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3,5000L));
-        env.setStateBackend(new FsStateBackend("hdfs://ambari01:8020/flinkCK/gmall"));
-        System.setProperty("HADOOP_USER_NAME", "hdfs");
 
+//        //状态后端设置
+//        env.enableCheckpointing(3000L, CheckpointingMode.EXACTLY_ONCE);
+//        env.getCheckpointConfig().setCheckpointTimeout(60 * 1000L);
+//        env.getCheckpointConfig().setMinPauseBetweenCheckpoints(3000L);
+//        env.getCheckpointConfig().enableExternalizedCheckpoints(
+//                CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION
+//        );
+//        env.setRestartStrategy(RestartStrategies.failureRateRestart(
+//                10, Time.of(1L, TimeUnit.DAYS), Time.of(3L, TimeUnit.MINUTES)
+//        ));
+//        env.setStateBackend(new HashMapStateBackend());
+//        env.getCheckpointConfig().setCheckpointStorage("hdfs://ambari01:8020/gmall/ck");
+//        System.setProperty("HADOOP_USER_NAME", "hdfs");
+
+        //TODO 2.读取Kafka topic_db 主题数据创建流
         String topic = "topic_db";
         String groupId = "dim_app_211126";
 
         DataStreamSource<String> kafkaDS = env.addSource(MyKafkaUtil.getFlinkKafkaConsumer(topic, groupId));
-        SingleOutputStreamOperator<JSONObject> filterDS = kafkaDS.flatMap(new FlatMapFunction<String, JSONObject>() {
+
+        //TODO 3.将数据转换为JSON对象并过滤   主流
+        OutputTag<String> dirtyTag = new OutputTag<String>("Dirty") {
+        };
+        SingleOutputStreamOperator<JSONObject> jsonObjDS = kafkaDS.process(new ProcessFunction<String, JSONObject>() {
             @Override
-            public void flatMap(String s, Collector<JSONObject> collector) throws Exception {
+            public void processElement(String value, Context ctx, Collector<JSONObject> out) throws Exception {
                 try {
-                    JSONObject jsonObject = JSON.parseObject(s);
-                    collector.collect(jsonObject);
+                    JSONObject jsonObject = JSON.parseObject(value);
+                    out.collect(jsonObject);
                 } catch (Exception e) {
-                    System.out.println("发现脏数据: " + s);
+                    ctx.output(dirtyTag, value);
                 }
             }
         });
+        jsonObjDS.getSideOutput(dirtyTag).print("Dirty>>>>>>");
 
-        MySqlSource<String> mySqlSource = MySqlSource.<String>builder().hostname("ambari01")
+        //TODO 4.使用FlinkCDC读取配置信息表创建配置流
+        MySqlSource<String> mySqlSource = MySqlSource.<String>builder()
+                .hostname("ambari01")
                 .port(3306)
                 .username("root")
                 .password("Hd7502002")
-                .databaseList("gmall")
+                .databaseList("gmall_config")
+                .tableList("gmall_config.table_process")
                 .startupOptions(StartupOptions.initial())
                 .deserializer(new JsonDebeziumDeserializationSchema())
                 .build();
+        DataStreamSource<String> mysqlDS = env.fromSource(
+                mySqlSource,
+                WatermarkStrategy.noWatermarks(),
+                "MysqlSource");
+        mysqlDS.print();
 
-        // 6.2 封装为流
-        DataStreamSource<String> mysqlDSSource = env.fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "MysqlSource");
+        //TODO 5.将配置信息流处理成广播流
+        MapStateDescriptor<String, TableProcess> stateDescriptor = new MapStateDescriptor<>("map-state", String.class, TableProcess.class);
+        BroadcastStream<String> broadcastStream = mysqlDS.broadcast(stateDescriptor);
 
-        // 6.3 广播配置流
-        MapStateDescriptor<String, TableProcess> tableConfigDescriptor = new MapStateDescriptor<String, TableProcess>("table-process-state", String.class, TableProcess.class);
-        BroadcastStream<String> broadcastDS = mysqlDSSource.broadcast(tableConfigDescriptor);
+        //TODO 6.连接主流与广播流
+        BroadcastConnectedStream<JSONObject, String> connectedStream = jsonObjDS.connect(broadcastStream);
 
-        // TODO 7. 连接流
-        BroadcastConnectedStream<JSONObject, String> connectedStream = filterDS.connect(broadcastDS);
+        //TODO 7.根据广播流数据处理主流数据
+        SingleOutputStreamOperator<JSONObject> hbaseDS = connectedStream.process(new TableProcessFunction(stateDescriptor));
+        hbaseDS.print(">>>>>>>>");
 
+        //TODO 8.将数据写出到Phoenix中
+        hbaseDS.addSink(new DimSinkFunction());
 
-        SingleOutputStreamOperator<JSONObject> dimDS = connectedStream.process(
-                new MyBroadcastFunction(tableConfigDescriptor)
-        );
-
-        // TODO 9. 将数据写入 Phoenix 表
-        dimDS.addSink(new MyPhoenixSink());
-
+        //TODO 9.启动任务
+        env.execute();
     }
 }
--- gmall-realtime/src/main/java/com/atguigu/app/func/TableProcessFunction.java
+++ gmall-realtime/src/main/java/com/atguigu/app/func/TableProcessFunction.java
@@ -1,168 +1,112 @@
 package com.atguigu.app.func;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidPooledConnection;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.atguigu.bean.TableProcess;
 import com.atguigu.common.GmallConfig;
+import com.atguigu.utils.DruidDSUtil;
 import org.apache.flink.api.common.state.BroadcastState;
 import org.apache.flink.api.common.state.MapStateDescriptor;
 import org.apache.flink.api.common.state.ReadOnlyBroadcastState;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.functions.co.BroadcastProcessFunction;
 import org.apache.flink.util.Collector;
-import org.apache.flink.util.OutputTag;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class TableProcessFunction extends BroadcastProcessFunction<JSONObject, String, JSONObject> {
 
-    //定义Phoenix连接
-    private Connection connection;
-
-    //定义侧输出流标签
-    private OutputTag<JSONObject> objectOutputTag;
-
-    //定义Map状态表述器
-    private MapStateDescriptor<String, TableProcess> mapStateDescriptor;
+    private MapStateDescriptor<String, TableProcess> stateDescriptor;
+    private DruidDataSource druidDataSource;
 
     public TableProcessFunction() {
     }
 
-    public TableProcessFunction(OutputTag<JSONObject> objectOutputTag, MapStateDescriptor<String, TableProcess> mapStateDescriptor) {
-        this.objectOutputTag = objectOutputTag;
-        this.mapStateDescriptor = mapStateDescriptor;
+    public TableProcessFunction(MapStateDescriptor<String, TableProcess> stateDescriptor) {
+        this.stateDescriptor = stateDescriptor;
     }
 
     @Override
     public void open(Configuration parameters) throws Exception {
-        Class.forName(GmallConfig.PHOENIX_DRIVER);
-        connection = DriverManager.getConnection(GmallConfig.PHOENIX_SERVER);
+        druidDataSource = DruidDSUtil.createDataSource();
     }
 
-    //处理广播过来的数据
+    //Value:{"before":null,"after":{"id":6,"tm_name":"长粒香","logo_url":"/static/default.jpg"},"source":{"version":"1.5.4.Final","connector":"mysql","name":"mysql_binlog_source","ts_ms":1658192885916,"snapshot":"false","db":"gmall-211227-flink","sequence":null,"table":"base_trademark","server_id":0,"gtid":null,"file":"","pos":0,"row":0,"thread":null,"query":null},"op":"r","ts_ms":1658192885916,"transaction":null}
     @Override
     public void processBroadcastElement(String value, Context ctx, Collector<JSONObject> out) throws Exception {
 
-        //1.将数据转换为JavaBean value:{"database":"gmall-","table":"table_process","type":"insert",data":{"":""},"before-data":{"":""}}
+        //1.获取并将数据转换为JavaBean对象
         JSONObject jsonObject = JSON.parseObject(value);
-        JSONObject data = jsonObject.getJSONObject("data");
-        TableProcess tableProcess = JSON.parseObject(data.toJSONString(), TableProcess.class);
+        TableProcess tableProcess = JSON.parseObject(jsonObject.getString("after"), TableProcess.class);
 
-        if (tableProcess != null) {
+        //2.建表
+        checkTable(tableProcess.getSinkTable(),
+                tableProcess.getSinkColumns(),
+                tableProcess.getSinkPk(),
+                tableProcess.getSinkExtend());
 
-            //2.校验表是否存在,如果不存在,则创建Phoenix表
-            if (TableProcess.SINK_TYPE_HBASE.equals(tableProcess.getSinkType())) {
-                checkTable(tableProcess.getSinkTable(),
-                        tableProcess.getSinkColumns(),
-                        tableProcess.getSinkPk(),
-                        tableProcess.getSinkExtend());
-            }
-
-            //3.将数据写入状态广播处理
-            BroadcastState<String, TableProcess> broadcastState = ctx.getBroadcastState(mapStateDescriptor);
-            String key = tableProcess.getSourceTable() + ":" + tableProcess.getOperateType();
-            broadcastState.put(key, tableProcess);
-        }
-
+        //3.写入状态
+        String key = tableProcess.getSourceTable();
+        BroadcastState<String, TableProcess> broadcastState = ctx.getBroadcastState(stateDescriptor);
+        broadcastState.put(key, tableProcess);
     }
 
-    //处理主流数据  jsonObject:{"database":"gmall-","table":"base_trademark","type":"insert",data":{"":""},"before-data":{"":""}}
-    @Override
-    public void processElement(JSONObject jsonObject, ReadOnlyContext ctx, Collector<JSONObject> out) throws Exception {
-
-        //1.提取状态数据
-        ReadOnlyBroadcastState<String, TableProcess> broadcastState = ctx.getBroadcastState(mapStateDescriptor);
-        String key = jsonObject.getString("tableName") + ":" + jsonObject.getString("type");
-        TableProcess tableProcess = broadcastState.get(key);
-
-        if (tableProcess != null) {
-
-            //2.根据配置信息过滤字段
-            JSONObject data = jsonObject.getJSONObject("data");
-            String sinkColumns = tableProcess.getSinkColumns();
-            filterColumn(data, sinkColumns);
-
-            //3.分流,将HBase数据写入侧输出流,Kafka数据写入主流
-            String sinkType = tableProcess.getSinkType();
-
-            //将待写入的维度表或者主题名添加至数据中,方便后续操作
-            jsonObject.put("sinkTable", tableProcess.getSinkTable());
-
-            if (TableProcess.SINK_TYPE_HBASE.equals(sinkType)) {
-                ctx.output(objectOutputTag, jsonObject);
-            } else if (TableProcess.SINK_TYPE_KAFKA.equals(sinkType)) {
-                out.collect(jsonObject);
-            }
-
-        } else {
-            System.out.println("配置信息中不存在Key：" + key);
-        }
-    }
-
-    /**
-     * 校验表是否存在,如果不存在,则创建Phoenix表
-     * <p>
-     * create table if not exists xx.xx(id varchar primary key,name varchar,sex varchar) xxx
-     *
-     * @param sinkTable   表名
-     * @param sinkColumns 列信息
-     * @param sinkPk      主键
-     * @param sinkExtend  扩展字段
-     */
+    //校验并建表 create table if not exists db.tn(id varchar primary key,name varchar,sex varchar) xxx
     private void checkTable(String sinkTable, String sinkColumns, String sinkPk, String sinkExtend) {
 
-        //处理主键以及扩展字段
-        if (sinkPk == null || sinkPk.equals("")) {
-            sinkPk = "id";
-        }
-        if (sinkExtend == null) {
-            sinkExtend = "";
-        }
-
-        //创建建表SQL
-        StringBuilder createTableSQL = new StringBuilder("create table if not exists ")
-                .append(GmallConfig.HBASE_SCHEMA)
-                .append(".")
-                .append(sinkTable)
-                .append("(");
-
-        //将建表字段拆开
-        String[] columns = sinkColumns.split(",");
-        for (int i = 0; i < columns.length; i++) {
-
-            String column = columns[i];
-
-            //如果当前字段为主键
-            if (sinkPk.equals(column)) {
-                createTableSQL.append(column).append(" varchar ").append("primary key");
-            } else {
-                createTableSQL.append(column).append(" varchar ");
-            }
-            //如果当前字段不是最后一个字段
-            if (i < columns.length - 1) {
-                createTableSQL.append(",");
-            }
-        }
-
-        //拼接扩展字段
-        createTableSQL.append(")").append(sinkExtend);
-
-        //打印SQL
-        String sql = createTableSQL.toString();
-        System.out.println(sql);
-
-        //执行SQL建表
+        DruidPooledConnection connection = null;
         PreparedStatement preparedStatement = null;
         try {
-            preparedStatement = connection.prepareStatement(sql);
+            if (sinkPk == null) {
+                sinkPk = "id";
+            }
+            if (sinkExtend == null) {
+                sinkExtend = "";
+            }
+
+            //创建建表SQL语句
+            StringBuilder sql = new StringBuilder("create table if not exists ")
+                    .append(GmallConfig.HBASE_SCHEMA)
+                    .append(".")
+                    .append(sinkTable)
+                    .append("(");
+            String[] columns = sinkColumns.split(",");
+            for (int i = 0; i < columns.length; i++) {
+                String column = columns[i];
+                if (sinkPk.equals(column)) {
+                    sql.append(column).append(" varchar primary key");
+                } else {
+                    sql.append(column).append(" varchar");
+                }
+
+                //如果不是最后一个字段,那么需要拼接","
+                if (i < columns.length - 1) {
+                    sql.append(",");
+                }
+            }
+            sql.append(")").append(sinkExtend);
+
+            //打印建表语句
+            System.out.println(">>>>>" + sql);
+
+            //编译SQL
+            connection = druidDataSource.getConnection();
+            preparedStatement = connection.prepareStatement(sql.toString());
+
+            //执行写入
             preparedStatement.execute();
+
         } catch (SQLException e) {
-            throw new RuntimeException("Phoenix建表失败！");
+            throw new RuntimeException("创建:" + sinkTable + "失败！");
         } finally {
+            //释放资源
             if (preparedStatement != null) {
                 try {
                     preparedStatement.close();
@@ -170,23 +114,48 @@ public class TableProcessFunction extends BroadcastProcessFunction<JSONObject, S
                     e.printStackTrace();
                 }
             }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
-    /**
-     * 根据配置信息过滤字段
-     *
-     * @param data        待过滤的数据
-     * @param sinkColumns 目标字段
-     */
-    private void filterColumn(JSONObject data, String sinkColumns) {
+    //value:{"database":"gmall","table":"cart_info","type":"update","ts":1592270938,"xid":13090,"xoffset":1573,"data":{"id":100924,"user_id":"93","sku_id":16,"cart_price":4488,"sku_num":1,"img_url":"http://47.93.148.192:8080/group1/M0rBHu8l-sklaALrngAAHGDqdpFtU741.jpg","sku_name":"华为 HUAWEI P40 麒麟990 5G SoC芯片 5000万30倍数字变焦 8GB+128GB亮黑色全网通5G手机","is_checked":null,"create_time":"2020-06-14 09:28:57","operate_time":null,"is_ordered":1,"order_time":"2021-10-17 09:28:58","source_type":"2401","source_id":null},"old":{"is_ordered":0,"order_time":null}}
+    @Override
+    public void processElement(JSONObject value, ReadOnlyContext ctx, Collector<JSONObject> out) throws Exception {
 
-        //1.处理目标字段
+        //1.获取广播数据
+        ReadOnlyBroadcastState<String, TableProcess> broadcastState = ctx.getBroadcastState(stateDescriptor);
+        TableProcess tableProcess = broadcastState.get(value.getString("table"));
+        String type = value.getString("type");
+
+        //2.过滤数据  行
+        if (tableProcess != null && ("insert".equals(type) || "update".equals(type) || "bootstrap-insert".equals(type))) {
+
+            //过滤数据 列
+            filterColumns(value.getJSONObject("data"), tableProcess.getSinkColumns());
+
+            //3.补充SinkTable字段写出
+            value.put("sinkTable", tableProcess.getSinkTable());
+            out.collect(value);
+        } else {
+            System.out.println("未找到对应的配置信息:" + value.getString("table") + ",或者类型错误:" + type);
+        }
+    }
+
+    //过滤数据 列
+    private void filterColumns(JSONObject data, String sinkColumns) {
+
         String[] columns = sinkColumns.split(",");
         List<String> columnList = Arrays.asList(columns);
 
-        //2.遍历data
         Set<Map.Entry<String, Object>> entries = data.entrySet();
         entries.removeIf(next -> !columnList.contains(next.getKey()));
+
     }
+
 }
